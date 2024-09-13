3cd508a3c6e9a3cdeeae6ad49917ae681913af9c
Alex Postolache <apostolache@harlemnext.com>
2024-09-13 10:38:23 +0200

    add demo page

--- demo/sketch.js
+++ demo/sketch.js
@@ -1,122 +1,146 @@
 let colorGen;
-let baseColorInput;
-let headerElement;
-let waveColors;
-let canvas;
+let waves;
+let currentColorScheme;
 
 function setup() {
-    // Set up canvas for the abstract art with waves
-    let canvasContainer = select('#canvas-container');
-    canvas = createCanvas(window.innerWidth, 400);
-    canvas.parent(canvasContainer); // Attach the canvas to the canvas container
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('waves-container');
+    colorMode(HSB, 360, 100, 100, 1);
+    
+    waves = new Waves();
+    
+    colorGen = new ColorGenerator(color('#ffba06'));
+    updateColorSchemes();
 
-    // Get references to the DOM elements
-    headerElement = select('#header');
-    baseColorInput = select('#baseColor');
-    baseColorInput.input(updateColors); // Update colors when the user changes the base color
-
-    // Set up an initial base color
-    colorGen = new ColorGenerator(baseColorInput.value());
-    updateColors();
+    let colorPicker = select('#colorPicker');
+    colorPicker.input(() => {
+        colorGen = new ColorGenerator(color(colorPicker.value()));
+        updateColorSchemes();
+    });
 }
 
-function updateColors() {
-    let baseColorValue = baseColorInput.value();
-    colorGen = new ColorGenerator(baseColorValue);
-
-    // Update the header background color
-    headerElement.style('background-color', baseColorValue);
-
-    // Update the wave colors with a new tetradic palette
-    waveColors = colorGen.getShades(5);
-
-    // Redraw the waves with the new palette
-    drawWaves();
-
-    // Update color schemes in the DOM
-    drawColorSchemes();
+function draw() {
+    clear();
+    waves.draw();
 }
 
-function drawWaves() {
-    var yScale = 120;
-    var xScale = 0.005;
-    var xTranslate = 0.2;
-    var itemsOffset = 1;
-    var lineGap = height / (waveColors.length + 1);
-    var numberOfCurves = waveColors.length;
-    var startColor = waveColors[0];
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function updateColorSchemes() {
+    updateColorDisplay('shades', colorGen.getShades(5));
+    updateColorDisplay('tints', colorGen.getTints(5));
+    updateColorDisplay('monochromatic', colorGen.getMonochromatic(5));
+    updateColorDisplay('complementary', colorGen.getComplementary());
+    updateColorDisplay('triadic', colorGen.getTriadic());
+    updateColorDisplay('analogous', colorGen.getAnalogous());
+    updateColorDisplay('splitComplementary', colorGen.getSplitComplementary());
+    updateColorDisplay('tetradic', colorGen.getTetradic());
     
-    colorFill = startColor;
-    background(startColor);
+    // Update waves colors
+    let tints = colorGen.getTints(5);
+    waves.updateColors(tints);
+}
+
+function updateColorDisplay(id, colors) {
+    let container = select(`#${id}`);
+    container.html('');
     
-    currentStartFrom = 0;
+    let schemeHeader = createDiv();
+    schemeHeader.class('scheme-header');
     
-    currentOffset = itemsOffset;
+    let schemeTitle = createElement('h2', id.charAt(0).toUpperCase() + id.slice(1));
+    schemeHeader.child(schemeTitle);
     
-    for (j = 0; j < numberOfCurves; j++) {
-        stroke(waveColors[j]);
+    let buttonContainer = createDiv();
+    buttonContainer.class('scheme-buttons');
+    
+    let copyButton = createButton('<i class="fas fa-copy"></i>');
+    copyButton.class('icon-button');
+    copyButton.attribute('data-tooltip', 'Copy to Clipboard');
+    copyButton.mousePressed(() => {
+        copyToClipboard(colors);
+        copyButton.attribute('data-tooltip', 'Copied!');
+        setTimeout(() => {
+            copyButton.attribute('data-tooltip', 'Copy to Clipboard');
+        }, 2000);
+    });
+    buttonContainer.child(copyButton);
+    
+    let useButton = createButton('<i class="fas fa-paint-brush"></i>');
+    useButton.class('icon-button');
+    useButton.attribute('data-tooltip', 'Use Scheme');
+    useButton.mousePressed(() => {
+        applyScheme(colors);
+    });
+    buttonContainer.child(useButton);
+    
+    schemeHeader.child(buttonContainer);
+    container.child(schemeHeader);
+    
+    let colorDisplay = createDiv();
+    colorDisplay.class('color-display');
+    colors.forEach(c => {
+        let swatch = createDiv();
+        swatch.class('color-swatch');
+        swatch.style('background-color', c);
+        colorDisplay.child(swatch);
+    });
+    container.child(colorDisplay);
+}
+
+function copyToClipboard(colors) {
+    let hexColors = colors.map(c => '#' + hex(red(c), 2) + hex(green(c), 2) + hex(blue(c), 2));
+    let colorArrayString = JSON.stringify(hexColors);
+    navigator.clipboard.writeText(colorArrayString);
+}
+
+function applyScheme(colors) {
+    waves.updateColors(colors);
+}
+
+class Waves {
+    constructor() {
+        this.colors = [];
+        this.yScale = 120;
+        this.xScale = 0.005;
+        this.xTranslate = 0.2;
+        this.itemsOffset = 1;
+        this.speed = 0.02;
+        this.startHeight = 1/5; 
+    }
+
+    updateColors(newColors) {
+        this.colors = newColors;
+    }
+
+    draw() {
+        let waveHeight = height * (1 - this.startHeight); // Height of the wave area
+        let lineGap = waveHeight / (this.colors.length + 1);
+        let numberOfCurves = this.colors.length;
+        let startColor = this.colors[0];
         
-        currentStartFrom += lineGap;
-        currentOffset += itemsOffset
+        let currentStartFrom = height * this.startHeight; // Start from 2/3 of the page height
+        let currentOffset = this.itemsOffset;
         
-        for (i = 0; i < width; i++) {
-          t = i * xScale + currentOffset;  
-          x = i;
-          y = sin(t+ xTranslate) * yScale;
-          y = map(y, 0, height, 0, yScale) + currentStartFrom;
-        
-          line(x,height, x, y);
+        for (let j = 0; j < numberOfCurves; j++) {
+            stroke(this.colors[j]);
+            
+            currentStartFrom += lineGap;
+            currentOffset += this.itemsOffset;
+            
+            for (let i = 0; i < width; i++) {
+                let t = i * this.xScale + currentOffset + frameCount * this.speed;
+                let x = i;
+                let y = sin(t + this.xTranslate) * this.yScale;
+                y = map(y, 0, height, 0, this.yScale) + currentStartFrom;
+                
+                // Ensure the line doesn't go above the start height
+                let topY = max(y, height * this.startHeight);
+                
+                line(x, height, x, topY);
+            }
         }
     }
-}
-
-function drawColorSchemes() {
-    // Get references to left and right columns
-    let leftColumn = select('#left-column');
-    let rightColumn = select('#right-column');
-
-    // Clear the previous content
-    leftColumn.html('');
-    rightColumn.html('');
-
-    // Complementary Color (Add base color in front)
-    leftColumn.child(createColorBox('Complementary', colorGen.getComplementary()));
-
-    // Triadic Colors (Add base color in front)
-    leftColumn.child(createColorBox('Triadic', colorGen.getTriadic()));
-
-    // Analogous Colors (Add base color in front)
-    leftColumn.child(createColorBox('Analogous', colorGen.getAnalogous()));
-
-    // Split Complementary Colors (Add base color in front)
-    rightColumn.child(createColorBox('Split Complementary', colorGen.getSplitComplementary()));
-
-    // Tetradic Colors (Add base color in front)
-    rightColumn.child(createColorBox('Tetradic', colorGen.getTetradic()));
-
-    // Monochromatic Colors
-    rightColumn.child(createColorBox('Monochromatic', colorGen.getMonochromatic(5)));
-    
-    // Shades
-    leftColumn.child(createColorBox('Shades', colorGen.getShades(8)));
-
-    // Tints
-    rightColumn.child(createColorBox('Tints', colorGen.getTints(8)));
-}
-
-function createColorBox(label, colors) {
-    let colorBox = createDiv();
-    colorBox.class('color-box');
-
-    let title = createElement('h3', label);
-    colorBox.child(title);
-
-    colors.forEach((col) => {
-        let colorBlock = createDiv();
-        colorBlock.class('color-block');
-        colorBlock.style('background-color', col);
-        colorBox.child(colorBlock);
-    });
-
-    return colorBox;
 }
